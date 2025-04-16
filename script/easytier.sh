77cbcdc7d72debb4430f488016954848e2847430
Hasenya7 <152851867+Hasenya7@users.noreply.github.com>
2025-04-16 15:46:41 +0800

    Update easytier.sh

--- script/easytier.sh
+++ script/easytier.sh
@@ -3,6 +3,7 @@ mkdir -p /etc/easytier && cd /etc/easytier
 #获取easyTier的Linux版本
 wget https://ghfast.top/https://github.com/EasyTier/EasyTier/releases/download/v2.2.4/easytier-linux-x86_64-v2.2.4.zip
 #解压并添加到bin目录
+unzip easytier-linux-x86_64-v2.2.4.zip
 mv easytier-linux-x86_64/* . && rm -rf easytier-linux-x86_64 easytier-linux-x86_64-v2.2.4.zip && chmod +x /* && mv ./easytier-* /usr/bin/
 #启动easytier,<网络名称>和<密码>需要自定义
 #nohup easytier-core  --network-name <网络名称> --network-secret <密码> > /etc/easytier/easytier.log 2>&1 &
