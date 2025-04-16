e2b7c105852c6a980c5f24e3984881adb2b6d9bc
Hasenya7 <152851867+Hasenya7@users.noreply.github.com>
2025-04-16 15:30:26 +0800

    Update easytier.sh

--- script/easytier.sh
+++ script/easytier.sh
@@ -1,6 +1,9 @@
-#用于重启或启动easytier.
-#关闭easytier进程
-ps -ef | grep '[e]asytier-core' | awk '{print $2}' | xargs -r kill -9
+#创建目录
+mkdir -p /etc/easytier && cd /etc/easytier
+#获取easyTier的Linux版本
+wget https://ghfast.top/https://github.com/EasyTier/EasyTier/releases/download/v2.2.4/easytier-linux-x86_64-v2.2.4.zip
+#解压并添加到bin目录
+mv easytier-linux-x86_64/* . && rm -rf easytier-linux-x86_64 easytier-linux-x86_64-v2.2.4.zip && chmod +x /* && mv ./easytier-* /usr/bin/
 #启动easytier,<网络名称>和<密码>需要自定义
 #nohup easytier-core  --network-name <网络名称> --network-secret <密码> > /etc/easytier/easytier.log 2>&1 &
 nohup easytier-core --network-name xiaoheihe --network-secret 147258 > /etc/easytier/easytier.log 2>&1 &
